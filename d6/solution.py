5964de75c68026b5405ddb8a5334c0a2b5b862f9
Baran Kesimal <dwlorp@gmail.com>
2019-12-12 14:19:14 -0500

    d6/p2

--- d6/solution.py
+++ d6/solution.py
@@ -43,36 +43,44 @@ def calculate_orbits_q1(objects: Dict):
 def calculate_orbits_q2(objects: Dict):
     can_reach_from = {}
 
-    def search_upwards(parent_key):
-        parent_count = next(can_reach_from[parent_key])
-        compute = 1 + parent_count
-        while True:
-            yield compute
-
-    def search_children(children_of_this_object, target):
-        can_reach_from_this_object = 0
+    def search_children(children_of_this_object, target_object):
+        can_reach_from_this_object = None
         for child_of_this_object in children_of_this_object:
             if target == child_of_this_object:
-                can_reach_from_this_object = 1
+                can_reach_from_this_object = 0
                 break
 
-            can_reach_from_child = next(can_reach_from[child_of_this_object])
-            if can_reach_from_child > 0:
+            reachability = can_reach_from.get(child_of_this_object)
+            if reachability is None:
+                break
+
+            can_reach_from_child = next(reachability[target_object])
+            if can_reach_from_child is not None:
                 can_reach_from_this_object = can_reach_from_child + 1
                 break
         while True:
             yield can_reach_from_this_object
 
     for key, children in objects.items():
-        for child in children:
-            can_reach_from[key] = {k: search_children(children, k) for k in ['SAN', 'YOU']}
+        can_reach_from[key] = {k: search_children(children, k) for k in ['SAN', 'YOU']}
 
-    diff = set(objects.keys()) - set(can_reach_from.keys())
+    minimum_so_far = None
+    for object_in_center, target_gens in can_reach_from.items():
+        total = None
+        for target, gen in target_gens.items():
+            required_steps = next(gen)
+            if required_steps is None:
+                total = None
+                break
+            total = total + required_steps if total else required_steps
+        if total is not None:
+            if minimum_so_far is None:
+                minimum_so_far = total
+            else:
+                minimum_so_far = min(total, minimum_so_far)
 
-    for key in diff:
-        assert(len(objects.get(key) or set()) == 0), "Incorrect # of leaves"
+    return minimum_so_far
 
-    return "in progress"
 
 # result = calculate_orbits_q1(process_input())
 result = calculate_orbits_q2(process_input())
